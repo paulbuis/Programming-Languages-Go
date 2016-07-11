a15ca8c6415baaee4e8eb38a02cc7db7641efc4b
Paul Buis <00pebuis@bsu.edu>
2016-07-11 11:57:28 -0400

    Added pop() to demo & added alternate Stack implementaion

--- stackdemo/stackdemo.go
+++ stackdemo/stackdemo.go
@@ -1,15 +1,39 @@
 package main
 
 import (
-    "fmt"
-    "stack"
+	"fmt"
+	"github.com/paulbuis/stack"
 )
 
 func main() {
-    st := stack.New()
-    st.push(1)
-    val, err := st.peek()
-    if err == nil {
-        fmt.println(st.peek())
-    }
+	fmt.Println("trying a SliceStack")
+	st0 := stack.NewSliceStack()
+	useStack(&st0)
+
+	fmt.Println("trying a ListStack")
+	st1 := stack.NewListStack()
+	useStack(&st1)
+
+}
+
+func useStack(s stack.Stack) {
+	s.Push(1)
+	s.Push(2)
+	val, err := s.Peek()
+	if err == nil {
+		fmt.Println(val)
+	} else {
+		return
+	}
+
+	val, err = s.Pop()
+	if err == nil {
+		fmt.Println(val)
+	} else {
+		return
+	}
+	val, err = s.Pop()
+	if err == nil {
+		fmt.Println(val)
+	}
 }
